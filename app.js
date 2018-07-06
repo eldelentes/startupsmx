ed03064747d40ceee150245e6068c37b1ffb4f27
Eduardo Higareda <eldelentes@gmail.com>
2018-07-05 20:40:56 -0500

    filtering

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+
 var app = new Vue({
     el: '#app',
     data: {
         items: [],
-        isActive: true
+        isActive: true,
+        currentCategory: ''
     },
     
     mounted: function(){
@@ -16,7 +18,7 @@ var app = new Vue({
             var app_id = "appn2FjzFlNxkwgjn";
             var app_key = "key2UzEvT5cWBP1zd";
             this.items = []
-        axios.get(
+            axios.get(
                 "https://api.airtable.com/v0/"+app_id+"/Table%201",
                 { 
                     headers: { Authorization: "Bearer "+app_key } 
@@ -30,6 +32,26 @@ var app = new Vue({
 
         close: function(){
             this.isActive = !this.isActive;
+        },
+
+        filter: function(e){
+            var currentCategory =  e.target.innerHTML
+            var currentItems = [];
+
+            for (i = 0; i < this.items.length; i++) { 
+                var item = this.items[i];
+                
+                for (j = 0; j < item.fields.Category.length; j++) { 
+                    if (item.fields.Category[j] == currentCategory) {
+                        currentItems.push(item)
+                        break
+                    }
+                }
+            }
+            this.items = currentItems;
+            console.log(currentCategory)
+            // this.items = this.items.filter(item => item.categories_.includes(categories, category));
         }
+
     }
 })
